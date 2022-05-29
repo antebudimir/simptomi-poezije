848deb935d9c87909f6a9dff59c9a55daef5e6f5
Ante Budimir <developer@antebudimir.com>
2022-05-29 11:01:16 +0300

    image src changed

--- src/components/SEO/index.jsx
+++ src/components/SEO/index.jsx
@@ -9,7 +9,7 @@ const SEO = ({ title, description, url }) => {
 
       <meta property="og:url" content={url} />
       <meta property="og:title" content={title} />
-      <meta property="og:image" content={`${siteUrl}/og-image.png`} />
+      <meta property="og:image" content={`${siteUrl}/static/og-image.jpg`} />
       {description && <meta name="description" content={description} />}
       {description && <meta property="og:description" content={description} />}
     </Helmet>
